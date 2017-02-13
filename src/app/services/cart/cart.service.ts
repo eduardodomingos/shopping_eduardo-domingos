0e14efc04c0d9005fab8024f2d9539bbaa36c7b5
Eduardo Domingos <epcdomingos@gmail.com>
2017-02-13 00:24:15 +0000

    Add service logic

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
+import { Product } from '../../models/product';
 
 @Injectable()
 export class CartService {
 
-  constructor() { }
+  constructor() {
+    this.products = [];
+  }
+
+  products: Product[];
+
+  add(product: Product) {
+    this.products.push(product);
+  }
+
+  remove(product: Product) {
+    const index = _.findIndex(this.products, {id:product.id});
+    this.products.splice(index, 1);
+  }
+
+  get() {
+    return this.products;
+  }
 
 }
