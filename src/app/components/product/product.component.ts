0763c054a5e376d4671402a24d159bc80e105ab6
Eduardo Domingos <epcdomingos@gmail.com>
2017-02-13 01:36:33 +0000

    Make component dynamic

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../models/product';
+import { CartService } from '../../services/cart/cart.service';
 
 @Component({
-  selector: 'app-product',
+  selector: 'article.product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  @Input() product;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
+  }
+
+  percentOff(product: Product) {
+    return Math.round(100*(product.priceOff/product.price));
+  }
+
+  addToCart(product: Product) {
+    this.cartService.add(this.product);
+  }
+
+  removeFromCart(product: Product) {
+    this.cartService.remove(this.product);
   }
 
 }
