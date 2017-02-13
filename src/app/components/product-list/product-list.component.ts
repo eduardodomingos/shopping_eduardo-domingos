5fe6c57c4631499e7f6d0f592f41f2f5db7b1b5d
Eduardo Domingos <epcdomingos@gmail.com>
2017-02-13 01:33:24 +0000

    Fetch and display products

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductDataService } from '../../services/data/product-data.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productDataService: ProductDataService) { }
 
   ngOnInit() {
+    this.productDataService.getProducts()
+      .subscribe(products => this.products = products);
   }
+
+  products: Product[];
 
 }
